e9a1b060558e9e3aa6ae9458889eb337d70ad3f4
Nkita777 <74130400+Nkita777@users.noreply.github.com>
2020-11-29 11:58:04 +0300

    tttru

--- 03.py
+++ 03.py
@@ -6,7 +6,9 @@ from random import randint
 bot = telebot.TeleBot("1459788300:AAHaJRpu3nstSTalXlYVkl3LedvNRzuYYvo")
 
 
-word = "антарктида"
+words = ["антарктида","пылесос","акваланг"]
+word = words[0]
+print(word)
 
 @bot.message_handler(commands = ['start'])
 def start_message(message):
@@ -16,12 +18,21 @@ def start_message(message):
 @bot.message_handler(content_types = ['text'])
 def otvet(message):
 
-    letter = message.text
-    if letter in word:
-        bot.send_message(message.chat.id, "Такая буква есть")
 
+    letter = message.text.lower()
+
+    if len(letter) == 1:
+        if letter in word:
+            bot.send_message(message.chat.id, "Такая буква есть")
+
+        else:
+            bot.send_message(message.chat.id, "Такой буквы нет")
     else:
-        bot.send_message(message.chat.id, "Такой буквы нет")
+        if letter == word:
+            bot.send_message(message.chat.id, "Вау ты угадал")
+        else:
+            bot.send_message(message.chat.id, "KAPPA OUTDATED  POGCHAMP OVERRATED  LONG HAVE WE WAITED  NOW YOU JEBAITED")
         
         
 bot.polling()
+
